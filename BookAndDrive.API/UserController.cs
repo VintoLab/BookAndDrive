7a1beb1e8793b30cab6a2a78eee6cff3676d0c8b
vaMoreth <jummyvasya24@gmail.com>
2025-02-12 18:02:45 +0100

    Add test UserController

---
+++ BookAndDrive.API/UserController.cs
@@ -0,0 +1,25 @@
+﻿using BookAndDrive.Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAndDrive.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _db.Users;
+            return Ok(users);
+        }
+    }
+}
