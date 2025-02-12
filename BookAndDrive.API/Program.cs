999def34e08ad8d2dfecc85e99ff33e5188ef5f7
vaMoreth <jummyvasya24@gmail.com>
2025-02-12 16:08:16 +0100

    Create and configurate ApplicationDbContext

--- BookAndDrive.API/Program.cs
+++ BookAndDrive.API/Program.cs
@@ -1,8 +1,15 @@
+using BookAndDrive.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp",
